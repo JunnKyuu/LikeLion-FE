8fc855641ad8009ba44b4cc790612d5cfc2a8b50
JunnKyuu <junnkyuu22@gmail.com>
2024-04-06 03:24:55 +0900

    State, Props practice

---
+++ vite-project-basic/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import HookExam from './components/HookExam';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <>
+      {/* <Register /> */}
+      <HookExam />
+    </>
+  );
+}
+
+export default App;
