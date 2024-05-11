e4a9cb25f7251f00aeb21b449628d2f38d53c303
JunnKyuu <junnkyuu22@gmail.com>
2024-05-12 00:39:22 +0900

    mockData 추가 및 todoItem border 조정

--- one-bite-react/todo-list/src/App.jsx
+++ one-bite-react/todo-list/src/App.jsx
@@ -14,13 +14,19 @@ const mockData = [
   {
     id: 1,
     isDone: false,
-    content: '풋살하기',
+    content: '넷플릭스 보기',
     date: new Date().getTime(),
   },
   {
     id: 2,
     isDone: false,
     content: '멋사 활동',
+    date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    isDone: false,
+    content: '수원삼성 경기보기',
     date: new Date().getTime(),
   },
 ];
