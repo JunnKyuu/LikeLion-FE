6228bdf861cca8041ad6123008ed2dbd3ca10da8
JunnKyuu <junnkyuu22@gmail.com>
2024-07-07 00:32:01 +0900

    MaterialUI 및 tailwindcss를 사용

--- one-bite-react/react-toyproject/src/App.jsx
+++ one-bite-react/react-toyproject/src/App.jsx
@@ -1,6 +1,54 @@
-import React from 'react';
 import { useState, useEffect } from 'react';
+import { MovieCard } from './components/MovieCard';
+import axios from 'axios';
+
+import { Container, Box } from '@mui/material';
 
 export const App = () => {
-  return <div>App</div>;
+  const [data, setData] = useState({ results: [] });
+
+  const token =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmUyOGEwNGYxNDA2ODIzNmU3ZjJkOGIxNWI0MDZjNCIsIm5iZiI6MTcyMDI3NDQ4My41Njc4OTYsInN1YiI6IjY2ODc4NGEyZDc4ODA3YjQ5NjkzNjkyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TpkCcILcwEBOi65_vErEqY8_2exzzrW2K5Zg-FY1xbA';
+
+  const getData = async () => {
+    try {
+      const result = await axios.get('https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1&region=KR', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setData(result.data);
+      console.log('success');
+    } catch {
+      console.log('error');
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <Container fixed maxWidth="sm">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <h1 className="text-3xl font-bold">현재 상영작</h1>
+        {data.results.map((item) => (
+          <MovieCard
+            key={item.id}
+            title={item.title}
+            backdrop_path={item.backdrop_path}
+            vote_average={item.vote_average}
+          />
+        ))}
+      </Box>
+    </Container>
+  );
 };
--- one-bite-react/react-toyproject/src/components/MovieCard.jsx
+++ one-bite-react/react-toyproject/src/components/MovieCard.jsx
@@ -1,10 +1,30 @@
-export const MovieCard = ({ poster_path, title, vote_average }) => {
-  const poster = 'https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1&region=KR' + poster_path;
+import { Grid } from '@mui/material';
+
+export const MovieCard = ({ title, backdrop_path, vote_average }) => {
+  const path = 'https://image.tmdb.org/t/p/w1280';
   return (
-    <div>
-      <img src={poster} alt="영화 포스터"></img>
-      <h4>{title}</h4>
-      <span>{vote_average}</span>
-    </div>
+    <Grid
+      container
+      sx={{
+        margin: 3,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: 'solid 1px #dddddd',
+        borderRadius: '10px',
+        padding: '20px',
+      }}
+    >
+      <Grid item>
+        <img className="rounded-md" src={path + backdrop_path} />
+      </Grid>
+      <Grid item>
+        <h3>{title}</h3>
+      </Grid>
+      <Grid item>
+        <p>평점: {vote_average}</p>
+      </Grid>
+    </Grid>
   );
 };
