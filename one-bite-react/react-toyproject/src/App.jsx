068787dbc4b48b1654aad02e6228716053a25968
JunnKyuu <junnkyuu22@gmail.com>
2024-07-10 00:03:38 +0900

    axios headers authorization 수정

--- one-bite-react/react-toyproject/src/App.jsx
+++ one-bite-react/react-toyproject/src/App.jsx
@@ -12,7 +12,7 @@ export const App = () => {
       const apiResult = await axios.get('https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1&region=KR', {
         headers: {
           Authorization:
-            'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmUyOGEwNGYxNDA2ODIzNmU3ZjJkOGIxNWI0MDZjNCIsIm5iZiI6MTcyMDI3NDQ4My41Njc4OTYsInN1YiI6IjY2ODc4NGEyZDc4ODA3YjQ5NjkzNjkyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TpkCcILcwEBOi65_vErEqY8_2exzzrW2K5Zg-FY1xbA',
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmUyOGEwNGYxNDA2ODIzNmU3ZjJkOGIxNWI0MDZjNCIsIm5iZiI6MTcyMDI3NDQ4My41Njc4OTYsInN1YiI6IjY2ODc4NGEyZDc4ODA3YjQ5NjkzNjkyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TpkCcILcwEBOi65_vErEqY8_2exzzrW2K5Zg-FY1xbA',
         },
       });
       console.log('success');
--- one-bite-react/react-toyproject/src/components/MovieCard.jsx
+++ one-bite-react/react-toyproject/src/components/MovieCard.jsx
@@ -4,8 +4,8 @@ export const MovieCard = ({ title, poster_path, vote_average }) => {
   const basicPath = 'https://image.tmdb.org/t/p/w1280';
 
   return (
-    <div className="grid place-items-center w-[450px] md:w-[680px] lg:w-[800px] bg-green-200 p-10 m-6">
-      <img className="mb-5" src={basicPath + poster_path} alt="영화 포스터" />
+    <div className="grid place-items-center w-[300px] md:w-[400px] lg:w-[500px] bg-green-200 p-10 m-6 rounded-[10px]">
+      <img className="mb-5 rounded-[10px]" src={basicPath + poster_path} alt="영화 포스터" />
       <h3 className="text-base font-bold md:text-lg lg:text-2xl ">{title}</h3>
       <p>{vote_average.toFixed(1)}</p>
     </div>
