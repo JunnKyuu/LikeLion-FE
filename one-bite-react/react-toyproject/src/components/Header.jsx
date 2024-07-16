5f60fb938cbd18f03de7693a6579cd54d2a63829
JunnKyuu <junnkyuu22@gmail.com>
2024-07-16 14:41:19 +0900

    home layout 설정

--- one-bite-react/react-toyproject/src/components/Header.jsx
+++ one-bite-react/react-toyproject/src/components/Header.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <header className="flex justify-between p-5">
-      <div className="flex justify-around w-[200px] bg-yellow-200">
+    <header className="flex justify-between p-5 bg-blue-200">
+      <div className="flex justify-around w-[200px] ">
         <Link to={'/'}>홈</Link>
         <Link to={'/movie'}>영화</Link>
         <Link to={'/my'}>마이페이지</Link>
       </div>
-      <div className="flex justify-around bg-green-200 w-[150px]">
+      <div className="flex justify-around w-[150px]">
         <Link to={'/login'}>로그인</Link>
         <Link to={'setting'}>설정</Link>
       </div>
--- one-bite-react/react-toyproject/src/pages/Home.jsx
+++ one-bite-react/react-toyproject/src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export const Home = () => {
-  return <div className="p-10 bg-green-200">Home</div>;
+  return (
+    <div className="p-10 bg-green-200">
+      <div className="grid place-items-center bg-yellow-200 h-[700px]">
+        <section className="w-[80%] h-[300px] bg-gray-300 p-5">section1</section>
+        <section className="w-[80%] h-[300px] bg-gray-300 p-5">section2</section>
+      </div>
+    </div>
+  );
 };
