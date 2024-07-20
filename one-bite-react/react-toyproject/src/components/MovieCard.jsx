6062f01425f8efb19b216848ea50e12d58c59d90
JunnKyuu <junnkyuu22@gmail.com>
2024-07-21 00:43:19 +0900

    movie carousel css 수정

--- one-bite-react/react-toyproject/src/components/MovieCard.jsx
+++ one-bite-react/react-toyproject/src/components/MovieCard.jsx
@@ -2,13 +2,13 @@ export const MovieCard = ({ title, poster_path, vote_average }) => {
   const basicUrl = 'https://image.tmdb.org/t/p/w1280';
 
   return (
-    <div>
-      <div>
-        <img src={basicUrl + poster_path} alt="poster" />
+    <div className="grid p-3 m-5 rounded-md place-items-center">
+      <div className="rounded-md">
+        <img className="m-1 rounded-md" src={basicUrl + poster_path} alt="poster" />
       </div>
-      <div>
-        <h4>{title}</h4>
-        <p>{vote_average}</p>
+      <div className="p-3">
+        <h4 className="font-GmarketBold">{title}</h4>
+        <p className="text-center font-GmarketMedium">{vote_average}</p>
       </div>
     </div>
   );
--- one-bite-react/react-toyproject/src/pages/MoviePage.jsx
+++ one-bite-react/react-toyproject/src/pages/MoviePage.jsx
@@ -27,13 +27,30 @@ export const MoviePage = () => {
   }, []);
 
   return (
-    <div className="grid place-items-center">
-      <div className="w-[80%]">
+    <div className="flex justify-center">
+      <div className="w-[500px] h-[800px] bg-blue-300 rounded-md p-5 m-2">
         {/* <Carousel className="rounded-xl">
         {data.results.map((item) => (
           <MovieCard key={item.id} title={item.title} poster_path={item.poster_path} vote_average={item.vote_average} />
         ))}
       </Carousel> */}
+        {/* <Carousel className="rounded-xl">
+          <img
+            src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+            alt="image 1"
+            className="object-cover w-full h-full"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+            alt="image 2"
+            className="object-cover w-full h-full"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+            alt="image 3"
+            className="object-cover w-full h-full"
+          />
+        </Carousel> */}
         <Carousel className="rounded-xl">
           {data.results.map((item) => (
             <MovieCard
